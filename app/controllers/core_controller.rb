b27eba651eee792f2b601c2a545f0b875ade66b1
Ryan Harrison <mail@ryanharrison.co.uk>
2013-11-07 14:09:53 +0000

    Added custom titles for each core controller action

--- app/controllers/core_controller.rb
+++ app/controllers/core_controller.rb
@@ -1,31 +1,41 @@
 class CoreController < ApplicationController
   def index
+    @title = "Home"
   end
 
   def about_the_club
+    @title = "About the Club"
   end
 
   def how_to_join
+    @title = "How to Join"
   end
 
   def what_you_need
+    @title = "What You Need"
   end
 
   def programme
+    @title = "2013 Programme"
   end
 
   def news_letters
+    @title = "News Letters"
   end
 
   def club_matters
+    @title = "Club Matters"
   end
 
   def how_to_get_there
+    @title = "How to get there"
   end
 
   def book_a_lawn
+    @title = "Book a Lawn"
   end
 
   def links
+    @title = "Links to other Croquet Sites"
   end
 end
