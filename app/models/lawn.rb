0a5663a6485299cab0bf8334cb7febf3fbc37ac3
Ryan Harrison <mail@ryanharrison.co.uk>
2013-12-01 13:18:12 +0000

    Removed lawn model

--- app/models/lawn.rb
+++
@@ -1,2 +0,0 @@
-class Lawn < ActiveRecord::Base
-end
--- db/migrate/20131127144546_create_lawns.rb
+++
@@ -1,9 +0,0 @@
-class CreateLawns < ActiveRecord::Migration
-  def change
-    create_table :lawns do |t|
-      t.boolean :booked
-
-      t.timestamps
-    end
-  end
-end
